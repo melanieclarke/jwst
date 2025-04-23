4f2f63a3c995d3a00251412033b0128310064520
Melanie Clarke <mclarke@stsci.edu>
2025-04-23 09:54:31 -0400

    Filter deprecation warnings to allow test input to be replaced with newer versions

--- jwst/regtest/test_nirspec_ifu_internal_cal.py
+++ jwst/regtest/test_nirspec_ifu_internal_cal.py
@@ -1,6 +1,7 @@
 """Test CubeBuildStep NIRSPEC internal_cal cubes"""
-import pytest
+import warnings
 
+import pytest
 from astropy.io.fits.diff import FITSDiff
 
 from jwst.stpipe import Step
@@ -18,7 +19,9 @@ def test_cube_build_nirspec_internal_cal(rtdata, fitsdiff_default_kwargs):
         '--save_results=true',
         '--coord_system=internal_cal'
     ]
-    with pytest.warns(DeprecationWarning):
+    with warnings.catch_warnings():
+        warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*Slit2Msa.*")
+        warnings.filterwarnings("ignore", category=DeprecationWarning, message=".*NIRSpec WCS.*")
         Step.from_cmdline(args)
 
     output = input_file.replace('cal', 'g395h-f290lp_internal_s3d')
